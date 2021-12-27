18c5d7598f05d7c233344e8daa41f7b889e9173d
Egor Ageev <48442694+ShonS1v4@users.noreply.github.com>
2021-12-27 14:26:43 +0300

    UPDATE
    1. ADD: DTO files
    2. ADD: Types of response
    3. ADD: Validation

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,5 +1,8 @@
 import { Body, Controller, Delete, Get, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import {CarDto} from "./dto/car.dto";
+import { KmDiscountDto } from './dto/km_discount.dto';
+import {RentDto} from "./dto/rent.dto";
 
 @Controller('cars')
 export class CarsController {
@@ -11,12 +14,12 @@ export class CarsController {
   }
 
   @Post()
-  public async setItem(@Body() data: any) {
+  public async setItem(@Body() data: CarDto) {
     return await this.serv.setItem(data);
   }
 
   @Post('/take')
-  public async takeForRent(@Body() data: any) {
+  public async takeForRent(@Body() data: RentDto) {
     return await this.serv.rent(data);
   }
 
@@ -27,22 +30,22 @@ export class CarsController {
 
   //tariffs and discounts
   @Get('/utils')
-  public async getUtils(@Body() data: any) {
+  public async getUtils(@Body() data: KmDiscountDto) {
     return await this.serv.getUtils(data.type);
   }
 
   @Post('/utils')
-  public async setUtils(@Body() data: any) {
+  public async setUtils(@Body() data: KmDiscountDto) {
     return await this.serv.setUtil(data);
   }
 
   @Delete('/utils')
-  public async deleteUtils(@Body() data: any) {
+  public async deleteUtils(@Body() data: KmDiscountDto) {
     return await this.serv.deleteUtil(data);
   }
 
   @Post('/utils/by')
-  public async getUtilsByRow(@Body() data: any) {
+  public async getUtilsByRow(@Body() data: KmDiscountDto) {
     return await this.serv.getUtilByRow(data);
   }
 
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import {HttpException, Injectable} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Cars } from './models/cars.entity';
 import { getRepository, Repository } from 'typeorm';
@@ -6,6 +6,9 @@ import { getConnection } from 'typeorm';
 import { Discount } from './models/discount.entity';
 import { Tariffs } from './models/tariffs.entity';
 import { Load } from './models/load.entity';
+import {CarDto} from "./dto/car.dto";
+import { RentDto } from './dto/rent.dto';
+import {KmDiscountDto} from "./dto/km_discount.dto";
 
 enum types {
   TARIFF,
@@ -25,100 +28,109 @@ export class CarsService {
     return await this.Cars.find();
   }
 
-  public async setItem(data) {
-    const carRepository = getRepository(Cars);
-    const loadRepository = getRepository(Load);
+  public async setItem(data: CarDto): Promise<Cars> {
+    try {
+      const carRepository = getRepository(Cars);
+      const loadRepository = getRepository(Load);
 
-    const car = new Cars();
-    car.mark = data.mark;
-    car.model = data.model;
-    car.gov_number = data.gov_number;
-    car.vin = data.vin;
+      const car = new Cars();
+      car.mark = data.mark;
+      car.model = data.model;
+      car.gov_number = data.gov_number;
+      car.vin = data.vin;
 
-    const load = new Load();
+      const load = new Load();
 
-    car.load_total = load;
+      car.load_total = load;
 
-    await carRepository.save(car);
-    await loadRepository.save(load);
+      await carRepository.save(car);
+      await loadRepository.save(load);
 
-    return car;
+      return car;
+    } catch (e) {
+      throw new HttpException(e, 400)
+    }
   }
 
-  public async rent(data: any) {
-    const car = await this.Cars.findOne({ id: data.id });
-    if (car.is_available)
+  public async rent(data: RentDto): Promise<object> {
+    try {
+      const car = await this.Cars.findOne({ id: data.id });
+      if (car.is_available)
         return  await this.rent_success(data)
-    if (!car.is_available)
-      return 'Машина на данный момент занята';
-    return 'Машина не найдена';
+      else
+        return {message: 'The car is currently busy'};
+    } catch (e) {
+        throw new HttpException(e, 400)
+    }
   }
 
   //rent
-  public async rent_success(data: any) {
-    //получение данных из тела запроса
-    const { taken_from, taken_for, km } = data;
-    //преобразование из строки в дату
-    const taken_from_day = new Date(taken_from).getDay();
+  public async rent_success(data: any): Promise<object>{
+    try {
+      //получение данных из тела запроса
+      const { taken_from, taken_for, km } = data;
+      //преобразование из строки в дату
+      const taken_from_day = new Date(taken_from).getDay();
 
-    //если начало аренды падает на субботу или воскресенье
-    if (taken_from_day == 6 || taken_from_day == 0) {
-      return 'Невозможно арендовать автомобиль в выходной день!';
+      //если начало аренды падает на субботу или воскресенье
+      if (taken_from_day === 6 || taken_from_day === 0) {
+        return {message: 'Impossible to rent a car on a weekend!'};
+      }
+      //подсчет общего срока аренды
+      const total_days = taken_for.split(' ')[2] - taken_from.split(' ')[2];
+
+      //если срок аренды не превышает максимально разрешимый
+      if (total_days <= 30) {
+        //получаем тариф подходящий под условие
+        const tariff = await this.getUtilByRow({ type: 0, km: km });
+        //получаем скику подходящую под условие аренды
+        const discount = await this.getUtilByRow({
+          type: 1,
+          days: total_days,
+        });
+        //высчитываем цену за день
+        const price_per_day =
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            //@ts-ignore
+            tariff.price - (tariff.price * discount.discount) / 100;
+
+        //высчитываем общюю цену за аренду
+        const total_price = price_per_day * total_days;
+
+        //получаем id отчета
+        const carRepository = getRepository(Cars);
+        const car = await carRepository.findOne(data.id, {
+          relations: ['load_total'],
+        });
+        car.load_total.total_km += km;
+        car.load_total.total_days += total_days;
+        car.load_total.last_rent_total = total_days;
+        car.load_total.last_km = km;
+        car.taken_from = taken_from;
+        car.taken_for = taken_for;
+        car.is_available = !car.is_available;
+        await carRepository.save(car);
+        return {
+          message: 'Rent success',
+          price: total_price,
+          load: car.load_total,
+        };
+      }
+      //если машину пытаются взять в аренду на срок больше максимального, то
+      return {message: 'The maximum rental period is up to 30 days!'};
+    } catch (e) {
+      throw new HttpException(e, 400)
     }
-    //подсчет общего срока аренды
-    const total_days = taken_for.split(' ')[2] - taken_from.split(' ')[2];
-
-    //если срок аренды не превышает максимально разрешимый
-    if (total_days <= 30) {
-      //получаем тариф подходящий под условие
-      const tariff = await this.getUtilByRow({ type: 0, km: km });
-      //получаем скику подходящую под условие аренды
-      const discount = await this.getUtilByRow({
-        type: 1,
-        days: total_days,
-      });
-      //высчитываем цену за день
-      const price_per_day =
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        //@ts-ignore
-        tariff.price - (tariff.price * discount.discount) / 100;
-
-      //высчитываем общюю цену за аренду
-      const total_price = price_per_day * total_days;
-
-      //получаем id отчета
-      const carRepository = getRepository(Cars);
-      const car = await carRepository.findOne(data.id, {
-        relations: ['load_total'],
-      });
-      car.load_total.total_km += km;
-      car.load_total.total_days += total_days;
-      car.load_total.last_rent_total = total_days;
-      car.load_total.last_km = km;
-      car.taken_from = taken_from;
-      car.taken_for = taken_for;
-      car.is_available = !car.is_available;
-      await carRepository.save(car);
-      return {
-        message: 'Rent success',
-        price: total_price,
-        load: car.load_total,
-      };
-    }
-
-    //если машину пытаются взять в аренду на срок больше максимального, то
-    return 'Максимальный срок аренды до 30 дней!';
   }
 
-  //функция которая оббновит отчет об машине
-  public async getOverviewByyCar(id) {
-    console.log(id);
+  //функция которая находит отчет об машине по ее id
+  public async getOverviewByyCar(id: number) {
     const loads = await getRepository(Load).find({
       relations: ['car'],
     });
     for (let item of loads) {
-      item.car.id == id
-      ?? item
+      if (item.car.id === id)
+      return item
     }
   }
 
@@ -127,7 +139,6 @@ export class CarsService {
     const today = new Date();
     const new_end_date = String(today).split(' ');
     new_end_date[2] = String(Number(new_end_date[2]) + Number(3));
-    console.log(new_end_date);
     await getConnection()
       .createQueryBuilder()
       .update(Cars)
@@ -142,15 +153,15 @@ export class CarsService {
 
   //tariffs and discounts get all
   public async getUtils(data) {
-    if (data.type == types.TARIFF || data == types.TARIFF)
+    if (data.type === types.TARIFF || data === types.TARIFF)
       return await this.Tariffs.find();
-    if (data.type == types.DISCOUNT || data == types.DISCOUNT)
+    if (data.type === types.DISCOUNT || data === types.DISCOUNT)
       return await this.Discount.find();
   }
 
   //add tariff or discount
   public async setUtil(data) {
-    if (data.type == types.TARIFF) {
+    if (data.type === types.TARIFF) {
       const km_per_day = Number(data.km_per_day);
       const price = Number(data.price);
       await getConnection()
@@ -165,7 +176,7 @@ export class CarsService {
         ])
         .execute();
     }
-    if (data.type == types.DISCOUNT) {
+    if (data.type === types.DISCOUNT) {
       const days_from = Number(data.days_from);
       const days_to = Number(data.days_to);
       const discount = Number(data.discount);
@@ -185,59 +196,68 @@ export class CarsService {
   }
 
   //this func find tariff by km or discount by days
-  public async getUtilByRow(data) {
-    if (data.type === types.TARIFF) {
-      const { km } = data;
-      let tariffs = await this.getUtils({ type: 0 });
-      tariffs.sort((a) => {
-        return a.km_per_day;
-      });
-      tariffs = tariffs.reverse();
-      for (const item of tariffs) {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        //@ts-ignore
-        if (km >= item.km_per_day) {
-          return item;
+  public async getUtilByRow(data: KmDiscountDto) {
+    try {
+      if (data.type === types.TARIFF) {
+        const { km } = data;
+        let tariffs = await this.getUtils({ type: 0 });
+        tariffs.sort((a) => {
+          return a.km_per_day;
+        });
+        tariffs = tariffs.reverse();
+        for (const item of tariffs) {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          //@ts-ignore
+          if (km >= item.km_per_day) {
+            return item;
+          }
         }
-      }
-      return {
-        massage: 'Tariff not found',
-      };
-    }
-    if (data.type == types.DISCOUNT) {
-      const { days } = data;
-      if (days < 3) {
         return {
-          message: 'No discount allowed',
+          massage: 'Tariff not found',
         };
       }
-      const discounts = await this.getUtils({ type: 1 });
-      discounts.sort(function (a, b) {
-        return a.days_from - b.days_from;
-      });
-      for (let i = 0; i < discounts.length; i++) {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        //@ts-ignore
-        if (days <= discounts[i].days_to) return discounts[i];
+      if (data.type === types.DISCOUNT) {
+        const { days } = data;
+        if (days < 3) {
+          return {
+            message: 'No discount allowed',
+          };
+        }
+        const discounts = await this.getUtils({ type: 1 });
+        discounts.sort(function (a, b) {
+          return a.days_from - b.days_from;
+        });
+        for (let i = 0; i < discounts.length; i++) {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          //@ts-ignore
+          if (days <= discounts[i].days_to) return discounts[i];
+        }
+        return {
+          massage: 'Discount not found',
+        };
       }
-      return {
-        massage: 'Discount not found',
-      };
+    } catch (e) {
+      throw new HttpException(e, 400)
     }
   }
 
   //this func delete your tariff or discount
-  public async deleteUtil(data) {
-    if (data.type == types.TARIFF) {
-      const type = 'Tariffs';
-      const id = data.id;
-      await CarsService.delete(type, id);
+  public async deleteUtil(data: KmDiscountDto) {
+    try {
+      if (data.type === types.TARIFF) {
+        const type = 'Tariffs';
+        const id = data.id;
+        await CarsService.delete(type, id);
+      }
+      if (data.type === types.DISCOUNT) {
+        const type = 'Discount';
+        const id = data.id;
+        await CarsService.delete(type, id);
+      }
+    } catch (e) {
+      throw new HttpException(e, 400)
     }
-    if (data.type == types.DISCOUNT) {
-      const type = 'Discount';
-      const id = data.id;
-      await CarsService.delete(type, id);
-    }
+
   }
 
   private static async delete(type, id) {
